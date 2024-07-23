647efa4ac67cb62915b22d5c9581e0f57726068e
Ana-Maria Draganuta <anamariadraganuta95@gmail.com>
2024-07-23 19:45:21 +0300

    Added Board component

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Board from "./Components/Board/Board";
 import Box from "./Components/Box/Box";
 
 function App() {
   return (
     <>
       <Box></Box>
+      <Board></Board>
     </>
   );
 }
---
+++ tic-tac-toe/src/Components/Board/Board.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import './Board.css';
+import Box from '../Box/Box';
+
+const Board = ({ boxes, onClick, disabled }) => {
+  return (
+    <div className="board">
+      {boxes.map((value, index) => (
+        <Box key={index} value={value} onClick={() => onClick(index)} disabled={disabled[index]} />
+      ))}
+    </div>
+  );
+};
+
+export default Board;
+
+Board.propTypes = {
+    boxes: PropTypes.array,
+    onClick: PropTypes.array,
+    disabled: PropTypes.array
+  };
